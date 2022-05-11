ac8b9c6d1d8d7ed91648f2cf60bb502846006243
Mithin <mithingthomas@gmail.com>
2022-05-11 15:07:00 -0400

    map notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
-    <div className="">
+    <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
