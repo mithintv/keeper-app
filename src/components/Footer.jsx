6cbffc51affa9ab85edbcfb7e1cfb16844a6e10f
Mithin <mithingthomas@gmail.com>
2022-05-11 15:02:00 -0400

    basic keeper layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Header() {
+  return (
+    <footer className="">
+      <p>Copyright &copy;{new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Header;
